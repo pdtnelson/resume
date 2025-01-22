49c37029d74490ce6ae06ef2b9b015bef2d49dae
Peter Nelson <pdtnelson@gmail.com>
2025-01-22 01:01:11 -0700

    .env support. MongoDB. API Updates

--- backend/modules/domain_models.py
+++ backend/modules/domain_models.py
@@ -1,13 +1,34 @@
-from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import BaseModel, Field
 
 
 class Profile(BaseModel):
+    id: str | None
     profile_img_url: str
     text: str
 
     @staticmethod
-    def from_dict(data):
+    def from_dict(id: str | ObjectId | None, data: dict):
         return Profile(
+            id=str(id) if id else None,
             profile_img_url=data["profile_img_url"],
             text=data["text"]
         )
+
+
+class Job(BaseModel):
+    title: str
+    start_date: date
+    end_date: date | None
+    duties: list[str]
+
+
+class Resume(BaseModel):
+    id: str | None
+    full_name: str
+    address_line_one: str
+    address_line_two: str
+    city: str
+    state: str
+    zip: int = Field(..., max=5)
+    jobs: list[Job]
--- backend/modules/profile.py
+++ backend/modules/profile.py
@@ -1,9 +1,31 @@
-from fastapi import APIRouter
+import logging
+
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
 from modules.domain_models import Profile
+from database.db import db
+
 router = APIRouter()
 
 
-@router.get("/profile")
+class CreateProfileRequest(BaseModel):
+    profile_img_url: str
+    text: str
+
+
+@router.get("/profiles")
 def get_profile():
     data = {"profile_img_url": "https://pdtnelson.com", "text": "Some text whoo!"}
     return Profile.from_dict(data)
+
+
+@router.post("/profile")
+def save_profile(profile: CreateProfileRequest):
+    try:
+        result = db['profiles'].insert_one(profile.model_dump())
+        new_profile = Profile.from_dict(result.inserted_id, profile.model_dump())
+        return new_profile
+    except BaseException as ex:
+        logging.exception(f"Error saving profile: {ex}")
+        raise HTTPException(status_code=500, detail="Error creating profile")
