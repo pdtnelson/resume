8eea934ff93dea32bab2b59431360e4d82e13828
Peter Nelson <pdtnelson@gmail.com>
2025-07-03 22:56:49 -0600

    Add blog to admin (#18)
    * Add API support for blog posts, update domain_models
    
    * Client side blog looks good
    
    * cleanup
    
    * PR Cleanup
    
    * Update admin panel for blog functions

--- frontend/src/components/Foot.vue
+++ frontend/src/components/Foot.vue
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon'
 </script>
 
 <template>
-  <footer class="bg-gray-900 text-white text-center p-8 mt-12 rounded-t-2xl shadow-inner">
+  <footer class="bg-gray-900 text-white text-center p-8 mt-12 shadow-inner">
     <p class="text-sm mb-2">&copy; {{ DateTime.now().year }} PDTNelson. All rights reserved.</p>
     <div class="mt-4 flex justify-center space-x-6">
       <a
